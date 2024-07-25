1fd31912053db5ca2f3d899fa048e490fb9a0806
ss <147905937+savan-25@users.noreply.github.com>
2024-07-25 18:01:17 +0530

    Update linearsearch.java

--- linearsearch.java
+++ linearsearch.java
@@ -26,3 +26,23 @@ public static int linear_search(int arr[],int target){
 
    }
  }
+   /// Q 1 SEARCH THE CHARACTER IN STRING
+class main{
+  public static void main(String[] args){
+    String name="savan";
+    char target='v';
+System.out.println(search(name,target));
+  }
+  static boolean search(String str,char target){
+    if (str.length()==0){
+      return false;
+    }
+     for (int i = 0; i < str.length(); i++) {
+      if(target==str.charAt(i)){
+        return true;
+      }
+      
+     }
+     return false;
+  }
+}
